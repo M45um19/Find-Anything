cb3ab4ae54cdeb80d11355a46e586c67f2e1c76b
HRahman1777 <hasiburrahman1313@gmail.com>
2021-06-22 02:41:54 +0600

    front_page_update - hasibur

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,84 +14,425 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
 
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <link rel="stylesheet" href="{{asset('asset/styles2.css')}}">
+  <link rel="stylesheet" href="{{asset('asset/res.css')}}">
+
   @livewireStyles
   <title>Find Anything</title>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-custom_nav">
-    <div class="container-fluid">
-      <a class="navbar-brand logo" href="#"><img class="logo2" src="{{asset('asset/image/2.png')}}" alt=""></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active text-primary nav_lnk" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-primary nav_lnk" href="#">Link</a>
-          </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-primary nav_lnk" href="#">About us</a>
-          </li>
+
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <nav class="navbar navbar-expand-lg custom_nav-container ">
+        <a class="navbar-brand" href="#">
+          <span>
+            Find Anything
+          </span>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav  ">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Shop
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+
+            @if(Route::has('login'))
+            @auth
+
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="fa fa-user"><span class="">{{Auth::user()->name}}</span> </i>
+              </a>
+
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="{{ url('/user/profile') }}">Profile</a></li>
+                <li><a class="dropdown-item" href="{{route('user.Shop')}}">My Shop</a></li>
+                @if(Auth::user()->utype === 'ADM')
+                <li><a class="dropdown-item" href="{{route('admin.dasboard')}}">Admin Panel</a></li>
+                @endif
+                <li>
+                  <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Log Out</a>
+                  <form id="logout-form" method="POST" action="{{ route('logout') }}">
+                    @csrf
+                  </form>
+                </li>
+              </ul>
+        </div>
+
+        @else
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('login')}}">
+            Login
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('register')}}">
+            Register
+          </a>
+        </li>
+        @endif
+        @endif
 
         </ul>
 
-        <span class="navbar-text">
-          @if(Route::has('login'))
-          @auth
-          @if(Auth::user()->utype === 'ADM')
-          <div class="dropdown">
-            <a class="dropdown-toggle log_in_link" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-              <i class="fa fa-user drop_username text-primary" style="font-size:25px;"><span class="drop_username">{{Auth::user()->name}}</span> </i>
-            </a>
+      </nav>
+    </header>
+    <!-- end header section -->
 
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <li><a class="dropdown-item text-primary" href="{{ url('/user/profile') }}">Profile</a></li>
-              <li><a class="dropdown-item text-primary" href="{{route('user.Shop')}}">My Shop</a></li>
-              <li><a class="dropdown-item text-primary" href="{{route('admin.dasboard')}}">Admin Panel</a></li>
-              <li>
-                <a class="dropdown-item text-primary" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Log Out</a>
-                <form id="logout-form" method="POST" action="{{ route('logout') }}">
-                  @csrf
-                </form>
-              </li>
+    <!-- slider section -->
 
-            </ul>
+    <section class="slider_section">
+      <div class="slider_container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-7">
+                    <div class="detail-box">
+                      <h1>
+                        Welcome To Our <br>
+                        Gift Shop
+                      </h1>
+                      <p>
+                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non
+                        necessitatibus error distinctio mollitia suscipit. Nostrum fugit
+                        doloribus consequatur distinctio esse, possimus maiores aliquid repellat
+                        beatae cum, perspiciatis enim, accusantium perferendis.
+                      </p>
+                      <a href="">
+                        Contact Us
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-5 ">
+                    <div class="img-box">
+                      <img src="{{asset('asset/image/1.png')}}" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item active">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-7">
+                    <div class="detail-box">
+                      <h1>
+                        Welcome To Our <br>
+                        Gift Shop
+                      </h1>
+                      <p>
+                        Sequi perspiciatis nulla reiciendis, rem, tenetur impedit, eveniet non
+                        necessitatibus error distinctio mollitia suscipit. Nostrum fugit
+                        doloribus consequatur distinctio esse, possimus maiores aliquid repellat
+                        beatae cum, perspiciatis enim, accusantium perferendis.
+                      </p>
+                      <a href="">
+                        Contact Us
+                      </a>
+                    </div>
+                  </div>
+                  <div class="col-md-5 ">
+                    <div class="img-box">
+                      <img src="{{asset('asset/image/1.png')}}" alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          @else
-          <div class="dropdown">
-            <a class="dropdown-toggle log_in_link" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-              <i class="fa fa-user drop_username text-primary" style="font-size:25px;"><span class="drop_username">{{Auth::user()->name}}</span> </i>
+          <div class="carousel_btn-box">
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span>&#60;</span>
             </a>
-
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <li><a class="dropdown-item text-primary" href="{{ url('/user/profile') }}">Profile</a></li>
-              <li><a class="dropdown-item text-primary" href="{{route('user.Shop')}}">My Shop</a></li>
-              <li>
-                <a class="dropdown-item text-primary" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Log Out</a>
-                <form id="logout-form" method="POST" action="{{ route('logout') }}">
-                  @csrf
-                </form>
-              </li>
-
-            </ul>
+            <img src="{{asset('asset/image/line.png')}}" alt="" />
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span>&#62;</span>
+            </a>
           </div>
-          @endif
+        </div>
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+  <!-- end hero area -->
 
-          @else
-          <a href="{{route('login')}}" class="log_reg fw-bold text-primary">Log in</a>
-          <a href="{{route('register')}}" class="log_reg fw-bold text-primary">Register</a>
-          @endif
-          @endif
-        </span>
+  <!-- shop section -->
 
+  <section class="shop_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Latest Ads
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="{{asset('asset/image/p5.png')}}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Shop Name
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="{{asset('asset/image/p5.png')}}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Shop Name
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="{{asset('asset/image/p5.png')}}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Shop Name
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="{{asset('asset/image/p5.png')}}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Shop Name
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="{{asset('asset/image/p5.png')}}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Shop Name
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="{{asset('asset/image/p5.png')}}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Shop Name
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="{{asset('asset/image/p5.png')}}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Shop Name
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="box">
+            <a href="">
+              <div class="img-box">
+                <img src="{{asset('asset/image/p5.png')}}" alt="">
+              </div>
+              <div class="detail-box">
+                <h6>
+                  Shop Name
+                </h6>
+              </div>
+              <div class="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+
+
+        <div class="btn-box">
+          <a href="">
+            View All Products
+          </a>
+        </div>
+      </div>
+  </section>
+
+  <!-- end shop section -->
+
+
+  <!-- info section -->
+
+  <section class="info_section  layout_padding2-top">
+    <div class="social_container">
+      <div class="social_box">
+        <a href="">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+        </a>
       </div>
     </div>
-  </nav>
+    <div class="info_container ">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-4">
+            <h6>
+              ABOUT US
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <h6>
+              NEED HELP
+            </h6>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed doLorem ipsum dolor sit amet,
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <h6>
+              CONTACT US
+            </h6>
+            <div class="info_link-box">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span> Dhaka, Bangladesh </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>+880123456789</span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span> findanything.info@gmail.com</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- footer section -->
+    <footer class=" footer_section">
+      <div class="container">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By
+          <a href="#">FindAnything</a>
+        </p>
+      </div>
+    </footer>
+    <!-- footer section -->
+
+  </section>
+
+  <!-- end info section -->
 
 
 
@@ -103,6 +444,13 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/glide.js"></script>
+
+
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+  <script src="{{asset('asset/js/custom.js')}}"></script>
 
 
   <script>
