5c963cb5d53c65abc28c0443b4bf867da5484562
HRahman1777 <hasiburrahman1313@gmail.com>
2021-06-22 20:22:31 +0600

    Update userDashboardBase.blade.php

--- resources/views/layouts/userDashboardBase.blade.php
+++ resources/views/layouts/userDashboardBase.blade.php
@@ -78,30 +78,9 @@
                         </div>
                     </li>
                     <li>
-                        <a class="nav-link px-3 sidebar-link" data-bs-toggle="collapse" href="#layouts">
-                            <span class="me-2"><i class="bi bi-layout-split"></i></span>
-                            <span>Layouts</span>
-                            <span class="ms-auto">
-                                <span class="right-icon">
-                                    <i class="bi bi-chevron-down"></i>
-                                </span>
-                            </span>
-                        </a>
-                        <div class="collapse" id="layouts">
-                            <ul class="navbar-nav ps-3">
-                                <li>
-                                    <a href="#" class="nav-link px-3">
-                                        <span class="me-2"><i class="bi bi-speedometer2"></i></span>
-                                        <span>Dashboard</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li>
                         <a href="#" class="nav-link px-3">
                             <span class="me-2"><i class="bi bi-book-fill"></i></span>
-                            <span>Pages</span>
+                            <span>My Products</span>
                         </a>
                     </li>
                     <li class="my-4">
