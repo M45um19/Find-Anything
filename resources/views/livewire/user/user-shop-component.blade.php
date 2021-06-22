0cb8f761c35e219d7e06adebf69e9087b002f494
Naim Istiak Masum <naim15-1793@diu.edu.bd>
2021-06-22 07:04:20 -0700

    shop model

--- resources/views/livewire/user/user-shop-component.blade.php
+++ resources/views/livewire/user/user-shop-component.blade.php
@@ -1,7 +1,7 @@
 <div>
     <main class="mt-5 pt-3">
         <div class="container-fluid">
-            <h1>User</h1>
+            <a class="btn btn-success">Add Shop details</a>
         </div>
     </main>
 </div>
