8dbaabc4d5428d4fe8241a926d7040d7eae1a1c0
HRahman1777 <hasiburrahman1313@gmail.com>
2021-06-25 21:42:19 +0600

    session alert edited

--- resources/views/layouts/adminDashboardBase.blade.php
+++ resources/views/layouts/adminDashboardBase.blade.php
@@ -140,14 +140,20 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @if(Session::has('Success_message'))
+    <p id="ses" hidden>
+        {{Session::get('Success_message')}}
+    </p>
     <script>
+        msg = $('#ses').text();
+        console.log(msg);
         Swal.fire({
-            position: 'top-end',
+            position: 'center',
             icon: 'success',
-            title: 'Your work has been saved',
+            title: msg,
             showConfirmButton: false,
-            timer: 1500
+            timer: 2000
         })
+    </script>
     </script>
     @endif
     @livewireScripts
